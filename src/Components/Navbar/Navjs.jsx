47114ed3e2135c04c4574d1b5514da89a9d7885c
Karan Pawar <110679627+Kreez81@users.noreply.github.com>
2023-04-16 21:44:39 +0530

    dropdown done

--- src/Components/Navbar/Navjs.jsx
+++ src/Components/Navbar/Navjs.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-scroll';
 import {FaBars} from "react-icons/fa";
 import './Navcss.css';
@@ -6,17 +6,23 @@ function Navbar() {
   const handleReload = () => {
     window.location.reload();
   };
+  const [openDropdown, setOpenDropdown] = useState(false);
   return (
     <div className='Navbody'>
         <button onClick={handleReload} className='NameH'>Yogchetna</button>
         <>
-        <FaBars className='dropbutt'/>
-        <div className='dropdown'>
+        <FaBars className='dropbutt' onClick={() => setOpenDropdown((prev) => !prev)}/>
+        {
+          openDropdown && 
+          <div className='dropdown'>
               <ul>
-                <li>About</li>
+                <li><Link to='secpage' smooth={true} duration={800} >About</Link></li>
                 <li>Contact</li>
               </ul>
         </div>
+          
+        }
+        
         </>
         <text className='Contact'>Contact</text>
         <text className='About'><Link to='secpage' smooth={true} duration={800} >About</Link></text>

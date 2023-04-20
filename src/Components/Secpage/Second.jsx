66150b646b58b5c948ea5958e5e9dfd1c6f55fa0
Karan Pawar <110679627+Kreez81@users.noreply.github.com>
2023-04-20 20:07:11 +0530

    logo and name

--- src/Components/Secpage/Second.jsx
+++ src/Components/Secpage/Second.jsx
@@ -4,9 +4,9 @@ import om from "../../Assets/om.png";
 function Second() {
   return (
     <div className='secbody' id='secpage'>
-       <img src={om} className="omimg" alt="logo" />
-       <div>
-        
+       {/* <img src={om} className="omimg" alt="logo" /> */}
+       <div className='Tagline-box'>
+        <line className='Tagline'>"Yoga is a skill in action"</line>
        </div>
     </div>
   )

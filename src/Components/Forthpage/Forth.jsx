0e5358e2d703a3edc38ecc5a4e9004b677efcd22
Karan Pawar <110679627+Kreez81@users.noreply.github.com>
2023-05-14 17:02:01 +0530

    contact page

--- src/Components/Forthpage/Forth.jsx
+++ src/Components/Forthpage/Forth.jsx
@@ -11,15 +11,15 @@ function Forth() {
 
               <div className='conbox'>
               <ul>
-                <li><FaPhoneAlt className='icon'/>&nbsp; +91 6263757605</li>
+                <li><FaPhoneAlt className='icon'/>&nbsp; +91-6263757605</li>
                 <li><a href='mailto:karan.p2001hello@gmail.com' target="_blank" rel="noopener noreferrer"><MdEmail className='icon'/>&nbsp;  chetna.pawar@gmail.com</a></li>
               </ul>
              </div>
              <div className='folbox'>
               <ul>
-                <li><FaInstagramSquare className='icon'/>&nbsp; Instagram</li>
-                <li><FaFacebookSquare className='icon'/>&nbsp; Facebook</li>
-                <li><a href='https://www.youtube.com/@yogchetna' target="_blank" rel="noopener noreferrer"><FaYoutube className='icon'/>&nbsp; Youtube</a></li>
+                <li className='social'><a href='https://www.instagram.com/yog.chetna/' target="_blank" rel="noopener noreferrer"><FaInstagramSquare className='icon'/>&nbsp; Instagram</a></li>
+                <li className='social'><a href='https://www.facebook.com/chetna.pawar' target="_blank" rel="noopener noreferrer"><FaFacebookSquare className='icon'/>&nbsp; Facebook</a></li>
+                <li className='social'><a href='https://www.youtube.com/@yogchetna' target="_blank" rel="noopener noreferrer"><FaYoutube className='icon'/>&nbsp; Youtube</a></li>
               </ul>
              </div>
              </section>
